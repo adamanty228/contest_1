78eda0269fb49e502b88245a5c5af15f86d4ae67
adamanty228 <77698115+adamanty228@users.noreply.github.com>
2021-08-30 11:06:42 +0300

    Update main.c

--- main.c
+++ main.c
@@ -34,14 +34,12 @@ void test()
 }
 
 int main() {
-    test();
-    // float a = extended_input('a');
-    // float b = extended_input('b');
-    // float c = extended_input('c');
-    // float ans_array[2] = {0};
-//
-    // int ans_count = solve(a, b, c, ans_array);
-    // output(ans_count, ans_array);
-//
+    // test();
+    float a = extended_input('a');
+    float b = extended_input('b');
+    float c = extended_input('c');
+    float ans_array[2] = {0};
+    int ans_count = solve(a, b, c, ans_array);
+    output(ans_count, ans_array);
     return 0;
 }
